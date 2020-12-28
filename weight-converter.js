742a2fc3425fde9de90423b90b5d803d105877fb
TillyBayBi <matilda_ako@hotmail.co.uk>
2020-12-28 15:03:10 +0000

    weight converter is working

---
+++ weight-converter.js
@@ -0,0 +1,7 @@
+var weight = prompt('Please enter your weight in pounds:');
+result = parseInt(weight) * 0.45359237;
+alert('Your weight in kilogram is: ' + result + 'kg');
+
+var checkStatus = result >= 60;
+
+alert('Based on your weight, it is ' + checkStatus + ' that you need to go on a diet');
