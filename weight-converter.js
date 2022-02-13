e39f33fca37510109f43a5bf31d1c6eeb5661f51
MatildaAko <matildaako@gmail.com>
2022-02-13 20:11:48 +0000

    Imperial BMI calculator is finished, cleaned up js file

--- weight-converter.js
+++ weight-converter.js
@@ -1,7 +1,7 @@
 //Metric and imperial buttons
-const metricWeightinput = document.querySelector("#weightkg");
+const metricWeightInput = document.querySelector("#weightkg");
 const metricHeightInput = document.querySelector("#heightcm");
-const imperialWeightinput = document.querySelector("#weightlb");
+const imperialWeightInput = document.querySelector("#weightlb");
 const imperialHeightInputFeet = document.querySelector("#heightft");
 const imperialHeightInputInches = document.querySelector("#heightin");
 //Both convert buttons
@@ -11,35 +11,100 @@ const calculateBtns = document.querySelectorAll(".calculate-btn");
 //Paragraphs to add text
 const metricParagraph = document.querySelector(".metric-paragraph");
 const imperialParagraph = document.querySelector(".imperial-paragraph");
-
-//Metric to imperial
-const convertedMetricWeight = (metricWeightValue / 0.45359237).toFixed(2);
-//Imperial to metric
-const convertedImperialWeight = (imperialWeightValue * 0.45359237).toFixed(2);
 //Converter
 convertBtns.forEach((convertBtn) => {
   convertBtn.addEventListener("click", (e) => {
     e.preventDefault();
     //Checks the button is in the metric form
     if (e.target.parentElement.classList.contains("metric-form")) {
-      const metricWeightValue = metricWeightinput.value;
+      const metricWeightValue = metricWeightInput.value;
       //Checks that the user has inputted a number
       if (metricWeightValue === "") {
         metricParagraph.innerHTML = "Please enter a number";
       } else {
+        //Metric to imperial
+        const convertedMetricWeight = (metricWeightValue / 0.45359237).toFixed(2);
         //Converting the number then puts it in the form
-        metricParagraph.innerHTML = `${metricWeightValue}kg is ${convertedMetricWeight}lb`;
-        metricWeightinput.value = "";
+        metricParagraph.innerHTML = `${metricWeightValue}kg is ${convertedMetricWeight}lbs`;
       }
     } else {
-      const imperialWeightValue = imperialWeightinput.value;
+      const imperialWeightValue = imperialWeightInput.value;
       if (imperialWeightValue === "") {
         imperialParagraph.innerHTML = "Please enter a number";
       } else {
+        //Imperial to metric
+        const convertedImperialWeight = (imperialWeightValue * 0.45359237).toFixed(2);
         //Converting the number then puts it in the form
         imperialParagraph.innerHTML = `${imperialWeightValue}lb is ${convertedImperialWeight}kg`;
-        imperialWeightinput.value = "";
       }
     }
+    clearAllFields();
+  });
+});
+
+function convertHeightToMetersSquared(cm) {
+  let meter = cm / 100;
+  return meter * meter;
+}
+
+function calculateHeightAsInches(feet, inches) {
+  let totalInches = parseFloat(feet * 12) + parseFloat(inches);
+  return totalInches * totalInches;
+}
+
+function calculateMetricBMI(weight, height) {
+  return (weight / height).toFixed(1);
+}
+
+function calculateImperialBMI(weight, height) {
+  return ((weight / height) * 703).toFixed(1);
+}
+
+function clearAllFields() {
+  metricWeightInput.value = "";
+  metricHeightInput.value = "";
+  imperialWeightInput.value = "";
+  imperialHeightInputFeet.value = "";
+  imperialHeightInputInches.value = "";
+}
+
+function bMIChecker(bmi) {
+  let metricBMIResult;
+  if (bmi <= 18.4) {
+    metricBMIResult = "Underweight";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    metricBMIResult = "Normal";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    metricBMIResult = "Overweight";
+  } else {
+    metricBMIResult = "Obese";
+  }
+  return metricBMIResult;
+}
+
+calculateBtns.forEach((calculateBtn) => {
+  calculateBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (e.target.parentElement.classList.contains("metric-form")) {
+      if (metricWeightInput === "" || metricHeightInput.value === "" || metricHeightInput.value === "") {
+        metricParagraph.innerHTML = "Please ensure all fields have been filled";
+      } else {
+        let metricBMI = calculateMetricBMI(metricWeightInput.value, convertHeightToMetersSquared(metricHeightInput.value));
+        let metricBMIResult = bMIChecker(metricBMI);
+        metricParagraph.innerHTML = `Your BMI is ${metricBMI}, based on the information you provided, this is considered: ${metricBMIResult}`;
+      }
+    } else {
+      if (imperialWeightInput === "" || imperialHeightInputFeet.value === "" || imperialHeightInputInches.value === "") {
+        imperialParagraph.innerHTML = "Please ensure all fields have been filled";
+      } else {
+        let imperialBMI = calculateImperialBMI(
+          imperialWeightInput.value,
+          calculateHeightAsInches(imperialHeightInputFeet.value, imperialHeightInputInches.value)
+        );
+        let imperialBMIResult = bMIChecker(imperialBMI);
+        imperialParagraph.innerHTML = `Your BMI is ${imperialBMI}, based on the information you provided, this is considered: ${imperialBMIResult}`;
+      }
+    }
+    clearAllFields();
   });
 });
