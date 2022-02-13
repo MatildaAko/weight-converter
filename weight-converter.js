7b4e36dc5fd1cea38a1bd7e2a97fa6e06fd2d03a
MatildaAko <matildaako@gmail.com>
2022-02-13 17:51:00 +0000

    JS for the convert buttons work, when clicked they take the input value and convert it, then dynamically displaying the result in the form

--- weight-converter.js
+++ weight-converter.js
@@ -1,11 +1,45 @@
-// var weight = prompt("Please enter your weight in pounds:");
-// result = parseInt(weight) * 0.45359237;
-// alert("Your weight in kilogram is: " + result + "kg");
+//Metric and imperial buttons
+const metricWeightinput = document.querySelector("#weightkg");
+const metricHeightInput = document.querySelector("#heightcm");
+const imperialWeightinput = document.querySelector("#weightlb");
+const imperialHeightInputFeet = document.querySelector("#heightft");
+const imperialHeightInputInches = document.querySelector("#heightin");
+//Both convert buttons
+const convertBtns = document.querySelectorAll(".convert-btn");
+//Both calculate buttons
+const calculateBtns = document.querySelectorAll(".calculate-btn");
+//Paragraphs to add text
+const metricParagraph = document.querySelector(".metric-paragraph");
+const imperialParagraph = document.querySelector(".imperial-paragraph");
 
-// var checkStatus = result >= 60;
-
-// alert(
-//     "Based on your weight, it is " +
-//     checkStatus +
-//     " that you need to go on a diet"
-// );
+//Metric to imperial
+const convertedMetricWeight = (metricWeightValue / 0.45359237).toFixed(2);
+//Imperial to metric
+const convertedImperialWeight = (imperialWeightValue * 0.45359237).toFixed(2);
+//Converter
+convertBtns.forEach((convertBtn) => {
+  convertBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    //Checks the button is in the metric form
+    if (e.target.parentElement.classList.contains("metric-form")) {
+      const metricWeightValue = metricWeightinput.value;
+      //Checks that the user has inputted a number
+      if (metricWeightValue === "") {
+        metricParagraph.innerHTML = "Please enter a number";
+      } else {
+        //Converting the number then puts it in the form
+        metricParagraph.innerHTML = `${metricWeightValue}kg is ${convertedMetricWeight}lb`;
+        metricWeightinput.value = "";
+      }
+    } else {
+      const imperialWeightValue = imperialWeightinput.value;
+      if (imperialWeightValue === "") {
+        imperialParagraph.innerHTML = "Please enter a number";
+      } else {
+        //Converting the number then puts it in the form
+        imperialParagraph.innerHTML = `${imperialWeightValue}lb is ${convertedImperialWeight}kg`;
+        imperialWeightinput.value = "";
+      }
+    }
+  });
+});
